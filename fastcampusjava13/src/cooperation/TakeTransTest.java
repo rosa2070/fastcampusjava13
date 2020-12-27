4f638856c3ad0c2c50fa2fd9618654493f21a3d3
jaeeunhyun <rosa2070@naver.com>
2020-12-27 12:45:16 +0900

    hahaheee

---
+++ fastcampusjava13/src/cooperation/TakeTransTest.java
@@ -0,0 +1,33 @@
+package cooperation;
+
+public class TakeTransTest {
+
+	public static void main(String[] args) {
+		Student studentJ = new Student("James", 5000);
+		Student studentT = new Student("Tomas", 10000);
+		Student studentE = new Student("Edward", 20000);
+		
+		Bus bus100 = new Bus(100);
+		Bus bus500 = new Bus(500);
+		Subway subwayGreen = new Subway(2);
+		Subway subwayBlue = new Subway(4);
+		Taxi taxi150 = new Taxi(150);
+		
+		
+		studentJ.takeBus(bus100);
+		studentT.takeSubway(subwayGreen);
+		studentE.takeTaxi(taxi150);
+		
+		studentJ.showInfo();
+		studentT.showInfo();
+		studentE.showInfo();
+		
+		bus100.showBusInfo();
+		bus500.showBusInfo();
+		
+		subwayGreen.showSubwayInfo();
+		taxi150.showTaxiInfo();
+
+	}
+
+}
---
+++ fastcampusjava13/src/cooperationquiz/Person.java
@@ -0,0 +1,14 @@
+package cooperationquiz;
+
+public class Person {
+
+	String personName;
+	int money;
+	
+	public Person(String personName, int money) {
+		this.personName = personName;
+		this.money = money;
+	}
+	
+	
+}
